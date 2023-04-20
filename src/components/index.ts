776c9f1d314d6a6f26a68f154ed009e7cdcb28bf
Osama Khan <osama6756@gmail.com>
2023-04-20 08:12:00 +0500

    Remove ProtocolDetail component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export { default as SolityLogo } from "./Logo";
 export { default as Dashboard } from "./Dashboard";
 export { default as Layout } from "./Layout";
-export { default as ProtocolDetail } from "./Protocol";
 export { default as DataTable } from "./DataTable";
 export { default as PoolDetail } from "./Pool";
 export { default as SubtleHeading } from "./SubtleHeading";
