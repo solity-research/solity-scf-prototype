c7a5b6b839717c691878179ff1a3030c8175be3a
Osama Khan <osama6756@gmail.com>
2023-04-20 08:10:38 +0500

    Add SubtleHeading export

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Dashboard } from "./Dashboard";
 export { default as Layout } from "./Layout";
 export { default as ProtocolDetail } from "./Protocol";
 export { default as DataTable } from "./DataTable";
+export { default as SubtleHeading } from "./SubtleHeading";
