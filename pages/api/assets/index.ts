465dc057051e7dcdb4260e8614da4d6ca9599783
Osama Khan <osama6756@gmail.com>
2023-04-20 13:27:26 +0500

    Add assets list API endpoint

---
+++ pages/api/assets/index.ts
@@ -0,0 +1,30 @@
+import { generateQueryString } from "@solity/helpers";
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  const { cursor, limit, order } = req.query;
+  try {
+    const params: Record<string, string> = {};
+    if (cursor) params.cursor = cursor as string;
+    if (limit) params.limit = limit as string;
+    params.order = (order || "desc") as string;
+
+    const query = generateQueryString(params);
+
+    const response = await axios.get(
+      "https://horizon.stellar.org/assets" + query
+    );
+    const {
+      _embedded: { records },
+      _links: { self, next, prev },
+    } = response.data;
+
+    res.status(200).json({ records, self, next, prev });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
